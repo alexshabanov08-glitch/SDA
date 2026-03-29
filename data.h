16f7821f2b32cc0a7869d5338a7690f16bc20b73
Oleksandr <alexshabanov08@gmail.com>
2026-03-29 12:07:36 +0300

    Update data.h

--- data.h
+++ data.h
@@ -6,16 +6,13 @@
 typedef struct {
     int id;
     char description[100];
-    char status[20]; // "в обработке" или "завершено"
+    char status[50];   /* "в обработке/завершено" = 18 симв. */
     char date[20];
 } Request;
 
-// функции
 void addRequest(Request arr[], int *size);
 void printRequests(Request arr[], int size);
 void saveToFile(Request arr[], int size);
 void loadFromFile(Request arr[], int *size);
-void searchById(Request arr[], int size);
-void editRequest(Request arr[], int size);
 
 #endif
